1f640b8779f589962bc242a3b146895f02f2ad97
Abhishek <codeabhishek18@gmail.com>
2024-12-30 20:54:36 +0530

    Layout updated

--- app/layout.js
+++ app/layout.js
@@ -23,9 +23,9 @@ export default async function RootLayout({ children })
   return (
     <html lang="en">
       <meta name="keywords" content="CAMS, CGSS, AML, Compliance"/>
-      <meta property="og:title" content="FCE&C - FinCrime Compliance Education & Consultancy"/>  
-      <meta property="og:description" content="FCE&Cs offers  high-quality materials, live interactive classes and practice sessions, all crafted by industry expert to clear CAMS & CGSS. Explore our resources and insights to combat financial crime effectively."/>
-      <meta property="og:image" content="fce&c"/>
+      <meta property="og:title" content="FINTS AML - FinCrime Trusted Source AML"/>  
+      <meta property="og:description" content="FINTS AML offers  high-quality materials, live interactive classes and practice sessions, all crafted by industry expert to clear CAMS & CGSS. Explore our resources and insights to combat financial crime effectively."/>
+      <meta property="og:image" content="fintsAML"/>
       {/* <meta property="og:url" content="https://www.fintsacademy.com"/> */}
       <SessionWrapper>
           <body className={poppins.className}>
